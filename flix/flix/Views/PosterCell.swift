a32fbcf88b8c5e5f39a515f882bc905cd9f3eed9
Manik Narang <narang_manik@rocketmail.com>
2018-02-12 21:14:50 -0800

    Assignment 2: flix

---
+++ flix/flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Manik Narang on 2/12/18.
+//  Copyright © 2018 Manik Narang. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
